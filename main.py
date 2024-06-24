59cd7869a1e6bae87078b663915227a25cee88c7
Walkline Wang <walkline@163.com>
2024-06-24 16:52:37 +0800

    增加主文件
    Signed-off-by: Walkline Wang <walkline@163.com>

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Copyright © 2024 Walkline Wang (https://walkline.wang)
+Gitee: https://gitee.com/walkline/micropython-ble-keypad
+"""
+from keypad.keypad import run_test
+
+if __name__ == '__main__':
+	run_test()
